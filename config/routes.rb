425cc50ac23745311a4d9f51942a0bf8874e0ce4
Justin Dao <tjd5at@virginia.edu>
2014-01-02 21:43:06 -0500

    Updates basic pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Justindao::Application.routes.draw do
   root to: "main#home"
+
+  get '/main_projects', to: "main#main_projects", :as => "main_projects"
+  get '/side_projects', to: "main#side_projects", :as => "side_projects"
+  get '/contact', to: "main#contact", :as => "contact"
 end
