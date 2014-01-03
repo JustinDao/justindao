97b5853352b423e8241ee2c0cd62a370869c36c7
Justin Dao <tjd5at@virginia.edu>
2014-01-02 21:03:21 -0500

    New Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Justindao::Application.config.secret_key_base = '3399d8889909c469344fc095d95c5ec5e3c5cc8f0c3f3beeee38818a65c5b7a0527d66c8d0142be36785912bc11812f2af7eff8befbf7ff8ea80c79a0a20bf8b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Justindao::Application.config.session_store :cookie_store, key: '_justindao_session'
